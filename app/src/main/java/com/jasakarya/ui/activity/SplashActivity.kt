e085fa4783c9173756804a3a4e5699458e6f487d
LoneSinx <lonesinx@gmail.com>
2023-12-04 22:15:54 +0700

    add splash activity

---
+++ app/src/main/java/com/jasakarya/ui/activity/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.jasakarya.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jasakarya.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
