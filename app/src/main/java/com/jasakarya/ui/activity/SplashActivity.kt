eed5b0e16e43f99feeea8f61cc286537e4ce5cd5
LoneSinx <lonesinx@gmail.com>
2023-12-04 22:26:27 +0700

    update splash activity, update main activity

--- app/src/main/java/com/jasakarya/ui/activity/SplashActivity.kt
+++ app/src/main/java/com/jasakarya/ui/activity/SplashActivity.kt
@@ -1,12 +1,32 @@
 package com.jasakarya.ui.activity
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import com.jasakarya.R
+import com.jasakarya.databinding.ActivitySplashBinding
 
+@SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
+    private var _splashBinding: ActivitySplashBinding? = null
+    private val splashBinding get() = _splashBinding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        _splashBinding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(splashBinding.root)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, delayMillis)
+    }
+
+    private companion object {
+        const val delayMillis = 3000L
     }
 }
