84bfc6a26e601aeffba82f2a0a14be65d617bc73
nubrandsta <ragasukma23@gmail.com>
2023-12-11 14:00:44 +0700

    Implemented local data persistent using shared preferences

---
+++ app/src/main/java/com/jasakarya/data/local/UserPreferences.kt
@@ -0,0 +1,33 @@
+package com.jasakarya.data.local
+
+import android.content.Context
+
+class UserPreferences(context: Context){
+    private val preferences = context.getSharedPreferences("userPref", Context.MODE_PRIVATE)
+
+    fun setUserId(userid:String){
+        val edit = preferences.edit()
+        edit.putString("userid",userid)
+        edit.apply()
+    }
+
+    fun setToken(token:String){
+        val edit = preferences.edit()
+        edit.putString("token",token)
+        edit.apply()
+    }
+
+    fun getUserId():String?{
+        return preferences.getString("userid",null)
+    }
+
+    fun getToken():String?{
+        return preferences.getString("token",null)
+    }
+
+    fun clearData(){
+        preferences.edit().clear().apply()
+    }
+
+
+}
